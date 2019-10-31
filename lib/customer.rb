4922873a6f0e5112b2aa8c5c3b0451b5dfcfb8d8
Pancho Labrador <plabradorjr@gmail.com>
2019-10-31 10:10:24 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -17,6 +17,11 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end
 
+  def new_meal_20_percent(waiter, total)
+      tip = total * 0.2
+      Meal.new(waiter, self, total, tip)
+  end
+
   def meals
     Meal.all.select do |meal|
       meal.customer == self
@@ -27,6 +32,18 @@ class Customer
     meals.map do |meal|
       meal.waiter
     end
+  end
+
+  def self.oldest_customer
+    oldest_age = 0
+    oldest_customer = nil
+    self.all.each do |customer|
+      if customer.age > oldest_age
+        oldest_age = customer.age
+        oldest_customer = customer
+      end
+    end
+    oldest_customer
   end
 
 end
